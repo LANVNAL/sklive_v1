39ec6a32e6aac1c12ee93991722b2d3e58c310d3
LANVNAL <503001939@qq.com>
2018-07-04 23:38:31 +0800

    demo

--- app.py
+++ app.py
@@ -1,13 +1,104 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField,SelectField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from qz_info import login,userinfo,curriculum,grade,exam_arrangement
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/database'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#db = SQLAlchemy(app)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class NameForm(FlaskForm):
+    func = SelectField('select function', choices=[('info', 'userinfo'), ('exam', 'exam'),('grade','grade'),('curriculum','curriculum')])
+    semester = SelectField('select semester',choices=[('2017-2018-1','2017-2018-1'),('2017-2018-2','2017-2018-2'),('2018-2019-1','2018-2019-1'),('2018-2019-2','2018-2019-2')])
+    username = StringField('Input your username', validators=[DataRequired()])
+    password = StringField('Input your password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class UserinfoForm(FlaskForm):
+    username = StringField('Input your username', validators=[DataRequired()])
+    password = StringField('Input your password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/qz')
+def qz():
+    username = '201601060210'
+    password = 'skctf2018'
+    semester = '2017-2018-2'
+    week = '12'
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    info = userinfo(username,headers,data)
+    return render_template('qz.html',info = info)
 
 
-if __name__ == '__main__':
-    app.run()
-x
+def get_info(username,password):
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    info = userinfo(username, headers, data)
+    return info
+
+def get_exam(username,password):
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    info = exam_arrangement(username,headers,data)
+    return info
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        func = form.func.data
+        semster = form.semester.data
+        username = form.username.data
+        password = form.password.data
+        if func == 'info':
+            info = userinfo(username, password)
+        elif func == 'exam':
+            info = exam_arrangement(username, password)
+        elif func == 'grade':
+            info = grade(username,password,semster)
+        print type(info),info
+        return render_template('qz.html',info = info)
+    else:
+        return render_template('index.html', form=form, name=session.get('name'))
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    form = UserinfoForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        info = userinfo(username, password)
+        #print type(info),info
+        return render_template('userinfo.html',info = info)
+    else:
+        return render_template('user.html', form=form, name=session.get('name'))
+
+app.run(host='0.0.0.0')
+
+
---
+++ qz_info.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import requests
+import json
+
+
+def login(username,password):
+    global S
+    login_url = 'http://jwgl.sdust.edu.cn/app.do?method=authUser&xh={}&pwd={}'.format(username,password)
+    S = requests.Session()
+    login = S.post(login_url)
+    j = json.loads(login.text)
+    token = j['token']
+    return token
+
+def userinfo(username,password):
+    global S
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    info_url = "http://jwgl.sdust.edu.cn:80/app.do?method=getUserInfo&xh={}".format(username)
+    userinfo = S.post(info_url, headers=headers, data=data)
+    return json.loads(userinfo.text)
+
+def curriculum(username,password,semester,week):
+    global S
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    curriculum_url = "http://jwgl.sdust.edu.cn/app.do?method=getKbcxAzc&xh={}&xnxqid={}&zc={}".format(username,semester,week)
+    curriculum = S.post(curriculum_url, headers=headers, data=data)
+    return json.loads(curriculum.text)
+
+
+def grade(username,password,semester):
+    global S
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    grade_url = 'http://jwgl.sdust.edu.cn/app.do?method=getCjcx&xh={}&xnxqid={}'.format(username,semester)
+    grade = S.post(grade_url, headers=headers, data=data)
+    return json.loads(grade.text)
+
+def exam_arrangement(username,password):
+    global S
+    token = login(username, password)
+    headers = {"token": token}
+    data = {"userAccountType": "2"}
+    exam_url = 'http://jwgl.sdust.edu.cn/app.do?method=getKscx&xh={}'.format(username)
+    exam_arrangement = S.post(exam_url, headers=headers, data=data)
+    #print exam_arrangement.text
+    return json.loads(exam_arrangement.text)
+
+if __name__ == '__main__':
+    username = '201601060210'
+    password = 'skctf2018'
+    semester = '2017-2018-2'
+    week = '12'
+    token = login(username,password)
+    headers = {"token":token}
+    data = {"userAccountType": "2"}
+    userinfo(username,headers,data)
+    curriculum(username,semester,week)
+    grade(username,semester)
+    exam_arrangement(username)

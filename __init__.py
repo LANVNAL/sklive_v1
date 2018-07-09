df86bde6dc88c05a51d43df2c16030378f2841e2
LANVNAL <503001939@qq.com>
2018-07-09 10:06:00 +0800

    demo1

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
+# -*- coding:utf-8 -*-
 from flask import Flask
 #from createdb import init_db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/sklive'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/sklive?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
